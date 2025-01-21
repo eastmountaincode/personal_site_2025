99ae683f149b55391760975f37d45b439ea23a3b
Andrew Boylan <andrewboylan@Andrews-MacBook-Pro-M3.local>
2025-01-21 13:26:18 -0500

    try spinner

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react'
 import Experience from './Experience/Experience'
 import ResumeHeader from './ResumeHeader'
+import profileImage from '../../assets/profile_square_2.jpg'
 
 function Resume() {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    const img = new Image()
+    img.src = profileImage
+    
+    img.onload = () => {
+      setIsLoaded(true)
+    }
+  }, [])
+
+  if (!isLoaded) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-purple-500"></div>
+      </div>
+    )
+  }
+
   return (
     <div>
       <ResumeHeader />
